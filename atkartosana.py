dac67657a862cb50f86314df631cf02d4d62d923
RalfsMazurs <78209314+RalfsMazurs@users.noreply.github.com>
2021-09-09 10:03:28 +0300

    9 septembra atkartosana

---
+++ atkartosana.py
@@ -0,0 +1,16 @@
+
+print("Teksta izdruka lapā")
+
+# Komentārs
+
+x = 1
+
+vards = "Mans vārds ir Ralfs"
+
+x = 0
+y = 5
+if x < y:  
+    print('x ir mazāks nekā y')
+
+if y < x: 
+    print('y ir mazāks nekā x')
